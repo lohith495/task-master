fd92d2c32b57f0a769c724a844ce98eac7534a72
Lohith <Lohith.Muduthanapelly@gmail.com>
2024-07-09 13:05:10 +0530

    commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = require('./routes/router');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", router);
 
-app.listen(port, (err) => {
+const server = app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
     }
     console.log(`Server is listening on ${port}`);
+    console.log(server.address());
 });
 
 
 
-module.exports = app;
+module.exports = server;
